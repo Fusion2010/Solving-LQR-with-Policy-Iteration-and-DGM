8220bf0721a23e8aaedc5e74c0615e5eb01afd96
aluo.n2010@gmail.com <aluo.n2010@gmail.com>
2023-04-05 18:30:48 +0100

    Plot 2.2

--- E2_2.py
+++ E2_2.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from E1_1 import SolveLQR
+from matplotlib import pyplot as plt
 
 from collections import namedtuple
 from typing import Tuple
@@ -9,7 +10,7 @@ from typing import Tuple
 
 class FFN(nn.Module):
 
-    def __init__(self, sizes, activation=nn.ReLU, output_activation=nn.Identity, batch_norm=False):
+    def __init__(self, sizes, activation = nn.ReLU, output_activation=nn.Identity, batch_norm=False):
         super().__init__()
 
         layers = [nn.BatchNorm1d(sizes[0]), ] if batch_norm else []
@@ -38,45 +39,86 @@ class FFN(nn.Module):
         return self.net(t_x)
 
 
+def train_policy(max_updates,
+                 layer_size,
+                 learning_rate=0.001,
+                 milestones=(10000,),
+                 gamma=0.1,
+                 batch_size=1000,
+                 loss_fn=nn.MSELoss(),
+                 **kwags
+                 ):
 
+    Net = FFN(layer_size)
 
-max_updates = 1000
-Net = FFN([3, 100, 2])
+    optimizer = torch.optim.Adam(Net.parameters(), lr = learning_rate)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones = milestones, gamma = gamma)
 
-optimizer = torch.optim.Adam(Net.parameters(), lr=0.001)
-scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=(10000,), gamma=0.1)
+    H = kwags.get('H')
+    M = kwags.get('M')
+    R = kwags.get('R')
+    C = kwags.get('C')
+    D = kwags.get('D')
+    T = kwags.get('T')
+    Sigma = kwags.get('Sigma')
+    LQR1 = SolveLQR(H, M, Sigma, C, D, R, T)
 
-loss_fn = nn.MSELoss()
+    running_loss = 0
+    episdoe = []
+    loss_eps = []
 
-H = np.identity(2)
-M = np.identity(2)
-R = np.identity(2)
-C = 0.1*np.identity(2)
-D = 0.1*np.identity(2)
-T = 1
-Sigma = np.diag([0.05, 0.05])
-LQR1 = SolveLQR(H, M, Sigma, C, D, R, T)
+    for it in range(max_updates):
+        optimizer.zero_grad()
 
-batch_size = 1000
-running_loss = 0
-for it in range(max_updates):
-    optimizer.zero_grad()
+        input_domain = (torch.rand(batch_size, 1, 2) - 0.5)*6
+        t = torch.from_numpy(np.linspace(0, 1, batch_size))
+        target_functional = LQR1.get_controller(t, input_domain).double()
 
-    input_domain = (torch.rand(batch_size, 1, 2) - 0.5)*6
-    t = torch.from_numpy(np.linspace(0, 1, batch_size))
-    target_functional = LQR1.get_controller(t, input_domain).double()
+        t_net = t.unsqueeze(1)
+        input_domain_net = input_domain.squeeze()
+        u_of_tx = Net(t_net, input_domain_net)
+        loss = loss_fn(u_of_tx, target_functional)
 
-    t_net = t.unsqueeze(1)
-    input_domain_net = input_domain.squeeze()
-    u_of_tx = Net(t_net, input_domain_net)
-    loss = loss_fn(u_of_tx, target_functional)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        running_loss += loss.item()
 
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-    running_loss += loss.item()
+        if (it + 1) % (max_updates // 20) == 0:  # print every 5%
+            mean_loss = running_loss / (max_updates // 20)
+            episdoe.append(it)
+            loss_eps.append(mean_loss)
+            print('[%5d] Training loss: %.9f' % (it + 1, mean_loss))
 
-    if it % 10 == 0:  # print every 2000 mini-batches
-        print('[%5d] loss: %.9f' %
-              (it + 1, running_loss / 10))
-        running_loss = 0.0
+            running_loss = 0.0
+
+    if kwags['visualize']:
+        plt.plot(episdoe, loss_eps)
+        plt.xlabel("Timesteps", fontsize = 20)
+        plt.ylabel("Averaged Loss", fontsize = 20)
+        plt.xticks(fontsize = 15)
+        plt.yticks(fontsize = 15)
+        plt.tight_layout(pad = 0.3)
+
+        plt.show()
+
+kwags = {
+    'H': np.identity(2),
+    'M': np.identity(2),
+    'R': np.identity(2),
+    'C': 0.1*np.identity(2),
+    'D': 0.1*np.identity(2),
+    'T': 1,
+    'Sigma': np.diag([0.05, 0.05]),
+    'visualize': True,
+}
+
+train_policy(max_updates = 1000,
+             layer_size = [3, 100, 2],
+             activation = 'Tanh',
+             learning_rate = 0.001,
+             milestones = (10000,),
+             gamma=0.1,
+             batch_size = 1000,
+             loss_fn = nn.MSELoss(reduction = 'mean'),
+             **kwags)
